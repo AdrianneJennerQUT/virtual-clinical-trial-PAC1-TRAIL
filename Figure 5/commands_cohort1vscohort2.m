157b3d60cc31bbe745e443adf3002531e6b30f0e
AdrianneJennerQUT <85978071+AdrianneJennerQUT@users.noreply.github.com>
2021-12-01 17:34:12 +1000

    Adding comments to code for Figure 5

--- Figure 5/commands_cohort1vscohort2.m
+++ Figure 5/commands_cohort1vscohort2.m
@@ -1,9 +1,12 @@
-% simulate average model behaviour
+% In this code, the dynamics of the cohorts for High PAC-1 elimination and Normal PAC-1
+% elimination virtual patients are examined in more detail
 
+%load virtual patient parameters
 load VP.mat
 load VP_May31.mat
 VirtualP=[VP; VP_May31];
 
+%load subcohort IDs
 load('cohort1_id.mat')
 cohort2_id = [];
 for ii = 1:400
@@ -15,8 +18,10 @@ end
 cohort1_parameters = VirtualP(cohort1_id,:);
 cohort2_parameters = VirtualP(cohort2_id,:);
 
+%% plotting cohort parameters
 
-% plotting cohort parameters
+% violin plot for Vd_PAC1 for patients in the High PAC-1 elimination cohort
+% (cohort 1) and the Normal PAC-1 elimination cohort (cohort 2)
 figure 
 v = violinplot([[cohort1_parameters(:,1);NaN(330,1)],cohort2_parameters(:,1)]);   
 ylabel('VD_{PAC1}')
@@ -28,7 +33,8 @@ v(1).ViolinAlpha = 0.5;
 v(2).ViolinAlpha = 0.5;
 set(gca,'xtick',[1 2],'xticklabels',{'Cohort 1','Cohort 2'})
 
-% plotting cohort parameters
+% violin plot for CL_PAC1 for patients in the High PAC-1 elimination cohort
+% (cohort 1) and the Normal PAC-1 elimination cohort (cohort 2)
 figure 
 v=violinplot([[cohort1_parameters(:,2);NaN(330,1)],cohort2_parameters(:,2)]);    
 ylabel('CL_{PAC1}')
@@ -40,7 +46,8 @@ v(1).ViolinAlpha = 0.5;
 v(2).ViolinAlpha = 0.5;
 set(gca,'xtick',[1 2],'xticklabels',{'Cohort 1','Cohort 2'})
 
-% plotting cohort parameters
+% violin plot for ke_PAC1 for patients in the High PAC-1 elimination cohort
+% (cohort 1) and the Normal PAC-1 elimination cohort (cohort 2)
 figure 
 v=violinplot([[cohort1_parameters(:,2)./cohort1_parameters(:,1);NaN(330,1)],cohort2_parameters(:,2)./cohort2_parameters(:,1)]);   
 ylabel('k_{ePAC1} (1/day}')
@@ -52,7 +59,8 @@ v(1).ViolinAlpha = 0.5;
 v(2).ViolinAlpha = 0.5;
 set(gca,'xtick',[1 2],'xticklabels',{'Cohort 1','Cohort 2'})
 
-% plotting cohort parameters
+% violin plot for Vd_TRAIL for patients in the High PAC-1 elimination cohort
+% (cohort 1) and the Normal PAC-1 elimination cohort (cohort 2)
 figure 
 v=violinplot([[cohort1_parameters(:,3);NaN(330,1)],cohort2_parameters(:,3)]);     
 ylabel('VD_{TRAIL}') 
@@ -64,7 +72,9 @@ v(1).ViolinAlpha = 0.5;
 v(2).ViolinAlpha = 0.5;
 set(gca,'xtick',[1 2],'xticklabels',{'Cohort 1','Cohort 2'})
 
-% plotting cohort parameters
+
+% violin plot for CL_TRAIL for patients in the High PAC-1 elimination cohort
+% (cohort 1) and the Normal PAC-1 elimination cohort (cohort 2)
 figure 
 v=violinplot([[cohort1_parameters(:,4);NaN(330,1)],cohort2_parameters(:,4)]);   
 ylabel('CL_{TRAIL}')
@@ -76,7 +86,8 @@ v(1).ViolinAlpha = 0.5;
 v(2).ViolinAlpha = 0.5;
 set(gca,'xtick',[1 2],'xticklabels',{'Cohort 1','Cohort 2'})
 
-% plotting cohort parameters
+% violin plot for ke_TRAIL for patients in the High PAC-1 elimination cohort
+% (cohort 1) and the Normal PAC-1 elimination cohort (cohort 2)
 figure 
 v=violinplot([[cohort1_parameters(:,4)./cohort1_parameters(:,3);NaN(330,1)],cohort2_parameters(:,4)./cohort2_parameters(:,3)]);   
 ylabel('k_{eTRAIL} (1/day}') 
@@ -88,7 +99,8 @@ v(1).ViolinAlpha = 0.5;
 v(2).ViolinAlpha = 0.5;
 set(gca,'xtick',[1 2],'xticklabels',{'Cohort 1','Cohort 2'})
 
-% plotting cohort parameters
+% violin plot for BW for patients in the High PAC-1 elimination cohort
+% (cohort 1) and the Normal PAC-1 elimination cohort (cohort 2)
 figure 
 v=violinplot([[cohort1_parameters(:,5);NaN(330,1)],cohort2_parameters(:,5)]);   
 ylabel('BW (kg)')
@@ -114,16 +126,15 @@ for i = 1:400
     
     sol = SolMat{i};
     time = linspace(0,p.tf,1000);
-    solind_C(i,:) = deval(sol,time,1);
-    solind_D(i,:) = deval(sol,time,2);
-    solind_PAC1(i,:) = deval(sol,time,3);
-    solind_TRAIL(i,:) = deval(sol,time,4);
+    solind_C(i,:) = deval(sol,time,1); % record total number of cancer cells
+    solind_PAC1(i,:) = deval(sol,time,3); % record total PAC-1 concentration
+    solind_TRAIL(i,:) = deval(sol,time,4); % record total TRAIL concentration
     
-    CumulativeTumourBurden_mat(i,:) = trapz(time,solind_C(i,:));
+    CumulativeTumourBurden_mat(i,:) = trapz(time,solind_C(i,:)); % record cumulative tumour burden
     
 end
 
-
+% plot histograms comparing cumulative tumour burden for each sub cohort
 figure
 histogram(log(real(CumulativeTumourBurden_mat(cohort1_id,:))),10,'FaceColor',[209,105,61]/255,'EdgeColor','none')
 hold on
@@ -135,6 +146,7 @@ box off
 legend('Cohort 1','Cohort 2')
 
 
+% plot comparing number of cancer cells for each individual in each sub cohort
 fig1 = figure
 hold on
 plot(time,solind_C(cohort1_id,:),'Color',[209,105,61]/255)
@@ -147,6 +159,7 @@ set(gca,'FontSize',18)
 set(gca,'yscale','log')
 legend([l1 l2],{'Cohort 1','Cohort 2'})
 
+% plot mean and std for number of cancer cells for each sub cohort
 fig1 = figure
 hold on
 options.handle = fig1;
@@ -166,24 +179,7 @@ ylabel('Cancer cells C, cells')
 set(gca,'FontSize',18)
 ylim([0 8e6])
 
-fig2 = figure
-hold on
-options.handle = fig2;
-options.color_area = [209,105,61]/255;
-options.color_line = [209,105,61]/255;
-options.alpha = 0.3;
-options.error = 'std';
-options.line_width = 2;
-options.x_axis = time;
-plot_areaerrorbar(real(solind_D(cohort1_id,:)),options)
-hold on
-options.color_area = [59 164 209]/255;
-options.color_line = [59 164 209]/255;
-plot_areaerrorbar(real(solind_D(cohort2_id,:)),options)
-xlabel('Time')
-ylabel('Dead cells D, cells')
-set(gca,'FontSize',18)
-
+% plot mean and std for PAC-1 for each sub cohort
 fig1 = figure
 hold on
 options.handle = fig1;
@@ -203,7 +199,7 @@ ylabel('PAC-1')
 set(gca,'FontSize',18)
 ylim([0 2500])
 
-
+% plot mean and std for TRAIL for each sub cohort
 fig1 = figure
 options.handle = fig1;
 options.color_area = [209,105,61]/255;
@@ -222,23 +218,24 @@ ylabel('TRAIL')
 set(gca,'FontSize',18)
 ylim([0 400])
 
-%% load optimal protocols and plot cohort dynamics
+%% load optimal protocols and plot cohort dynamics for High PAC-1 elimination cohort and 50 virtual patients from Normal PAC-1 elimination cohort
 load('Results_Nov8')
 
 OptimalDose = Results{end}.OptimalDose;   
 
+%setting colour map for the heatmap
+sumX = [247,252,240
+224,243,219
+204,235,197
+168,221,181
+123,204,196
+78,179,211
+43,140,190
+8,104,172
+8,64,129]/255;
 
-sumX = [0 18 25
-    0 95 115
-    10 147 150
-    148 210 189
-    233 216 166
-    238 155 0
-    202 103 2
-    187 62 3
-    174 32 18
-    155 34 38]/255;
-
+%plotting optimal PAC-1 treatment for patients in the patients in the High PAC-1
+%elimination sub cohort
 figure
 h = heatmap(OptimalDose(cohort1_id,1:11),'CellLabelColor','none','Colormap',sumX,'GridVisible','off')
 ylabel('Virtual Patients')
@@ -256,15 +253,16 @@ h.XDisplayLabels(8) = {"7"}
 h.XDisplayLabels(9) = {"8"}
 h.XDisplayLabels(10) = {"9"}
 h.XDisplayLabels(11) = {"10"}
-
 h.YDisplayLabels(1:length(cohort1_id)) = {""}
 
 s = struct(h);
 s.XAxis.TickLabelRotation = 0;  % vertical
 title('PAC-1')
 
+%plotting optimal PAC-1 treatment for the first 50 patients in the patients in the Normal PAC-1
+%elimination sub cohort
 figure
-h = heatmap(OptimalDose(cohort2_id,1:11),'CellLabelColor','none','Colormap',sumX,'GridVisible','off')
+h = heatmap(OptimalDose(cohort2_id(1:50),1:11),'CellLabelColor','none','Colormap',sumX,'GridVisible','off')
 ylabel('Virtual Patients')
 xlabel('Weekly dose')
 title('PAC-1')
@@ -280,12 +278,14 @@ h.XDisplayLabels(8) = {"7"}
 h.XDisplayLabels(9) = {"8"}
 h.XDisplayLabels(10) = {"9"}
 h.XDisplayLabels(11) = {"10"}
-h.YDisplayLabels(1:length(cohort2_id)) = {""}
+h.YDisplayLabels(1:50) = {""}
 
 s = struct(h);
 s.XAxis.TickLabelRotation = 0;  % vertical
 title('Cohort 2')
 
+%plotting optimal TRAIL treatment for patients in the patients in the High PAC-1
+%elimination sub cohort
 figure
 h = heatmap(OptimalDose(cohort1_id,12:22),'CellLabelColor','none','Colormap',sumX,'GridVisible','off')
 ylabel('Virtual Patients')
@@ -310,8 +310,10 @@ s = struct(h);
 s.XAxis.TickLabelRotation = 0;  % vertical
 title('PAC-1')
 
+%plotting optimal TRAIL treatment for the first 50 patients in the patients in the Normal PAC-1
+%elimination sub cohort
 figure
-h = heatmap(OptimalDose(cohort2_id,12:22),'CellLabelColor','none','Colormap',sumX,'GridVisible','off')
+h = heatmap(OptimalDose(cohort2_id(1:50),12:22),'CellLabelColor','none','Colormap',sumX,'GridVisible','off')
 ylabel('Virtual Patients')
 xlabel('Weekly dose')
 title('PAC-1')
@@ -321,13 +323,14 @@ h.XDisplayLabels(2) = {"1"}
 h.XDisplayLabels(3) = {"2"}
 h.XDisplayLabels(4) = {"3"}
 h.XDisplayLabels(5) = {"4"}
+
 h.XDisplayLabels(6) = {"5"}
 h.XDisplayLabels(7) = {"6"}
 h.XDisplayLabels(8) = {"7"}
 h.XDisplayLabels(9) = {"8"}
 h.XDisplayLabels(10) = {"9"}
 h.XDisplayLabels(11) = {"10"}
-h.YDisplayLabels(1:length(cohort2_id)) = {""}
+h.YDisplayLabels(1:50) = {""}
 
 s = struct(h);
 s.XAxis.TickLabelRotation = 0;  % vertical
